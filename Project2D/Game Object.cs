2586e511f611c727b35a6e478b8e2136813a5fae
Brad Jones <braddj2002@gmail.com>
2021-03-23 16:27:40 +1100

    Moving Project
    Moved Project into version control

---
+++ Project2D/Game Object.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib;
+using static Raylib.Raylib;
+using MathClasses;
+
+namespace Project2D
+{
+    class GameObject
+    {
+        protected GameObject Parent;
+        protected Matrix3 LocalTranform;
+        protected Matrix3 GlobalTransform;
+        protected Image logo;
+        protected Texture2D texture;
+        protected static List<GameObject> ChildrenList = new List<GameObject>();
+        
+
+        static void GetParent()
+        {
+
+        }
+
+        static void AddChild(GameObject child)
+        {
+            ChildrenList.Add(child);
+        }
+
+        static void RemoveChild(GameObject child)
+        {
+            ChildrenList.Remove(child);
+        }
+
+        public static void SetPosition(Vector2 PlayerPos)
+        {
+            PlayerPos.x = 0;
+            PlayerPos.y = 0;
+        }
+
+        public static Vector2 GetPosition()
+        {
+            return ;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Project2D/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib;
+using static Raylib.Raylib;
+using MathClasses;
+
+namespace Project2D
+{
+    class Player : GameObject
+    {
+        Image logo;
+        Texture2D texture;
+        public override void Update()
+        {
+            
+            float Speed = 10f;
+            Vector2 objectPos;
+            objectPos.x = 0;
+            objectPos.y = 0;
+            Vector2 objectVelocity;
+            objectVelocity.x = 0;
+            objectVelocity.y = 0;
+            if (IsKeyDown(KeyboardKey.KEY_W))
+            {
+                Vector2 force;
+                force.x = 0f;
+                force.y = -Speed;
+
+                objectVelocity.x += force.x * deltaTime;
+                objectVelocity.y *= objectVelocity.y * force.y;
+
+                Vector2 displacement;
+                displacement.x = objectVelocity.x * deltaTime;
+                displacement.y = objectVelocity.y * deltaTime;
+
+
+                objectPos.x += displacement.x;
+                objectPos.y += displacement.y;
+
+            }
+        }
+    }
+}
+
